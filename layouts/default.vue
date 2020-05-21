c6cd41363653aea97308f731db107982f3a615c3
akinov <ayumoto10@gmail.com>
2020-05-21 21:00:53 +0900

    init

---
+++ layouts/default.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <nav class="navbar header has-shadow is-primary" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="/">Jira2MD</a>
+      </div>
+    </nav>
+
+    <section class="main-content">
+      <nuxt />
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          title: "Home",
+          icon: "home",
+          to: { name: "index" }
+        },
+        {
+          title: "Inspire",
+          icon: "lightbulb",
+          to: { name: "inspire" }
+        }
+      ]
+    };
+  }
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="section">
+    <div class="columns">
+      <div class="column is-6">
+        <textarea></textarea>
+      </div>
+
+      <div class="column is-6">
+        <div class="container"></div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Card from "~/components/Card";
+
+export default {
+  name: "HomePage",
+
+  components: {
+    Card
+  }
+};
+</script>
